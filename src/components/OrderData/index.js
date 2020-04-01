e2d1b7693ab63d4c8cd3b5854413cf4d05074857
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-01 11:51:47 -0300

    Created order data

--- src/components/OrderData/index.js
+++ src/components/OrderData/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Section, Subtitle } from '../Section';
+import { Container, Title, Group, Text } from './styles';
 
 export default function OrderData() {
-  return <div />;
+  return (
+    <Section>
+      <Title>Dados do Pedido</Title>
+
+      <Container>
+        <Group>
+          <Subtitle>Comprado em</Subtitle>
+          <Text>05 de Fevereiro de 2019, às 19h30</Text>
+        </Group>
+        <Group>
+          <Subtitle>Ponto de Venda</Subtitle>
+          <Text>E-commerce</Text>
+        </Group>
+        <Group>
+          <Subtitle>Modalidade de Entrega</Subtitle>
+          <Text>F1 Envio pela loja, F2 Retira em Loja</Text>
+        </Group>
+      </Container>
+    </Section>
+  );
 }
---
+++ src/components/OrderData/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+import { Title as SectionTitle } from '../Section';
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Group = styled.div`
+  margin-right: 5rem;
+`;
+
+export const Title = styled(SectionTitle)`
+  margin-bottom: 0px;
+`;
+
+export const Text = styled.h4`
+  font-size: 0.75rem;
+  margin: 0px;
+  margin-top: 0.5rem;
+`;
